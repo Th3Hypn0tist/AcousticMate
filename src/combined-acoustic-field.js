18866d977b226b6a0eb79ea814f4bdcfa6b766b8
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-08-27 22:57:39 +0300

    Use hybrid modal and directional direct field

--- src/combined-acoustic-field.js
+++ src/combined-acoustic-field.js
@@ -1,3 +1,5 @@
+import { DirectSoundField } from './direct-sound-field.js';
+import { HybridAcousticField } from './hybrid-acoustic-field.js';
 import { RectangularRoomField } from './rectangular-room-field.js';
 
 function uniqueSpeakers(speakers = [], speakerSets = []) {
@@ -16,8 +18,9 @@ class FrequencySliceField {
     this.bounds = owner.domain.bounds;
     this.range = [0, Infinity];
   }
-  sample(x, y, z) { return this.owner.roomField.sampleAtFrequency(x, y, z, this.frequency); }
-  sampleComplex(x, y, z) { return this.owner.roomField.sampleComplexAtFrequency(x, y, z, this.frequency); }
+  sample(x, y, z) { return this.owner.hybridField.sampleAtFrequency(x, y, z, this.frequency); }
+  sampleComplex(x, y, z) { return this.owner.hybridField.sampleComplexAtFrequency(x, y, z, this.frequency); }
+  sampleComponents(x, y, z) { return this.owner.hybridField.sampleComponentsComplexAtFrequency(x, y, z, this.frequency); }
 }
 
 class PhaseAwareFrequencyField {
@@ -28,12 +31,23 @@ class PhaseAwareFrequencyField {
     this.frequency = null;
     this.frequencyRange = [...owner.frequencyRange];
   }
-  sample(x, y, z, frequencyHz) { return this.owner.roomField.sampleAtFrequency(x, y, z, frequencyHz); }
-  sampleComplex(x, y, z, frequencyHz) { return this.owner.roomField.sampleComplexAtFrequency(x, y, z, frequencyHz); }
+  sample(x, y, z, frequencyHz) { return this.owner.hybridField.sampleAtFrequency(x, y, z, frequencyHz); }
+  sampleComplex(x, y, z, frequencyHz) { return this.owner.hybridField.sampleComplexAtFrequency(x, y, z, frequencyHz); }
+  sampleComponents(x, y, z, frequencyHz) { return this.owner.hybridField.sampleComponentsComplexAtFrequency(x, y, z, frequencyHz); }
 }
 
 class CombinedAcousticField {
-  constructor({ domain, modes = null, speakers = [], speakerSets = [], frequencyRange = [20, 200], crossoverNetwork = null, q = 18, speedOfSound = 343 } = {}) {
+  constructor({
+    domain,
+    modes = null,
+    speakers = [],
+    speakerSets = [],
+    frequencyRange = [20, 200],
+    crossoverNetwork = null,
+    q = 18,
+    speedOfSound = 343,
+    hybrid = {},
+  } = {}) {
     if (!domain?.dimensions) throw new Error('CombinedAcousticField currently requires a solver-ready rectangular AcousticDomain');
     this.domain = domain;
     this.modes = modes == null ? [] : [...modes];
@@ -54,6 +68,8 @@ class CombinedAcousticField {
       q,
       speedOfSound,
     });
+    this.directField = new DirectSoundField({ speakers, speakerSets, crossoverNetwork, speedOfSound });
+    this.hybridField = new HybridAcousticField({ modalField: this.roomField, directField: this.directField, ...hybrid });
     this.frequencyField = new PhaseAwareFrequencyField(this);
   }
 
@@ -73,10 +89,10 @@ class CombinedAcousticField {
     if (!Number.isFinite(minHz) || !Number.isFinite(maxHz) || minHz < 0 || maxHz < minHz) throw new Error('CombinedAcousticField range requires 0 <= min <= max');
     this.frequencyRange = [minHz, maxHz];
     this.frequencyField.frequencyRange = [...this.frequencyRange];
-    if (Math.abs(this.roomField.maxFrequency - maxHz) > 1e-9) {
+    this.hybridField.frequencyRange = [...this.frequencyRange];
+    if (!this.externalModes && Math.abs(this.roomField.maxFrequency - maxHz) > 1e-9) {
       this.roomField.maxFrequency = maxHz;
-      if (this.externalModes) this.roomField.invalidate();
-      else this.roomField.rebuildModes();
+      this.roomField.rebuildModes();
     }
     return this;
   }
@@ -85,6 +101,7 @@ class CombinedAcousticField {
     this.speakers = speakers;
     this.speakerSets = speakerSets;
     this.roomField.setSpeakers(uniqueSpeakers(speakers, speakerSets));
+    this.directField.setSpeakers(speakers, speakerSets);
     return this;
   }
 
